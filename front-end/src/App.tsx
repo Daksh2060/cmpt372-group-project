42fb7d3c22feb8bd70cdde355d80176c7a47c325
Daksh Patel <daksh21697@gmail.com>
2024-03-17 06:17:17 -0700

    Refactoring

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,8 +1,8 @@
 //import {BrowserRouter, Route, Routes} from "react-router-dom";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MapComponent from "./map.tsx"
-import HomePage from "./home.tsx"
+import MapComponent from "./map.tsx";
+import HomePage from "./home.tsx";
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
         <div className="content">
           <Routes>
             <Route path="/" element={<HomePage />}></Route>
-            <Route path="/map" element={<MapComponent/>}></Route>
+            <Route path="/map" element={<MapComponent />}></Route>
           </Routes>
         </div>
       </div>
--- front-end/src/home.tsx
+++ front-end/src/home.tsx
@@ -1,21 +1,20 @@
 import { useNavigate } from "react-router";
 
 const HomePage = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const handleMap = () => {
+    navigate("/map");
+  };
 
-    const handleMap = () => {
-        navigate('/map');
-    }
-    
-    return (
+  return (
     <div>
-        <h1>Homepage</h1>
-        <div className="new">
-            <button onClick={handleMap}>GO TO MAP</button>
-        </div>
+      <h1>Homepage</h1>
+      <div className="new">
+        <button onClick={handleMap}>GO TO MAP</button>
+      </div>
     </div>
-    );
+  );
 };
 
 export default HomePage;
